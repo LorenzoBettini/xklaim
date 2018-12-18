1b5fa3a96eec24da8ef8e5edc1aff9a64040fd89
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2018-12-18 10:53:19 +0100

    another Hello World in Klava

---
+++ xklaim/xklaim.parent/klava.tests/src/examples/java/klava/examples/hello/KlavaHelloWorld2.java
@@ -0,0 +1,38 @@
+package klava.examples.hello;
+
+import klava.KString;
+import klava.KlavaException;
+import klava.PhysicalLocality;
+import klava.Tuple;
+import klava.topology.ClientNode;
+import klava.topology.KlavaNode;
+import klava.topology.KlavaProcess;
+import klava.topology.Net;
+
+public class KlavaHelloWorld2 {
+
+	public static void main(String[] args) throws Exception {
+		PhysicalLocality serverLoc = new PhysicalLocality("tcp-127.0.0.1:9999");
+		KlavaNode serverNode = new Net(serverLoc);
+
+		/* will automatically log to the server */
+		KlavaNode clientNode = new ClientNode(serverLoc);
+
+		serverNode.addNodeProcess(new KlavaProcess() {
+			@Override
+			public void executeProcess() throws KlavaException {
+				Tuple tuple = new Tuple(new Object[] {String.class});
+				in(tuple, self);
+				System.out.println("in: " + tuple.getItem(0));
+			}
+		});
+		
+		clientNode.addNodeProcess(new KlavaProcess() {
+			@Override
+			public void executeProcess() throws KlavaException {
+				out(new Tuple(new Object[] {"Hello World!"}), serverLoc);
+			}
+		});
+	}
+
+}
