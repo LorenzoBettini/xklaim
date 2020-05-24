f3c9454814c29b1d1d69e55c79152802562b8438
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2020-05-24 16:34:57 +0200

    just a file to check it compiles

--- xklaim/xklaim.parent/xklaim.example.maven/src/main/java/xklaim/example/maven/AnExample.java
+++ xklaim/xklaim.parent/xklaim.example.maven/src/main/java/xklaim/example/maven/AnExample.java
@@ -1,5 +1,14 @@
 package xklaim.example.maven;
 
-public class AnExample {
+import klava.topology.KlavaProcess;
 
+public class AnExample {
+	KlavaProcess process = new KlavaProcess() {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void executeProcess() {
+			// just to check whether it compiles
+		}
+	};
 }
