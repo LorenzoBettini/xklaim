66a3e8def6ebe64083f7ee2a2b5e7d768bf78630
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2026-04-14 14:27:34 +0200

    Fix outline string representation to handle Windows line endings

--- xklaim/xklaim.parent/xklaim.ui.tests/src/xklaim/ui/tests/XklaimOutlineTest.java
+++ xklaim/xklaim.parent/xklaim.ui.tests/src/xklaim/ui/tests/XklaimOutlineTest.java
@@ -1,12 +1,12 @@
 package xklaim.ui.tests;
 
-import org.eclipse.xtend2.lib.StringConcatenation;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.XtextRunner;
+import org.eclipse.xtext.ui.editor.outline.IOutlineNode;
 import org.eclipse.xtext.ui.testing.AbstractOutlineTest;
-import org.eclipse.xtext.xbase.lib.Exceptions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import xklaim.ui.internal.XklaimActivator;
 
 /**
@@ -21,6 +21,15 @@ public class XklaimOutlineTest extends AbstractOutlineTest {
 		return XklaimActivator.XKLAIM_XKLAIM;
 	}
 
+	/**
+	 * We must make sure to get rid of "\r" because in Windows Java text blocks do
+	 * not contain "\r" and the comparison would fail.
+	 */
+	@Override
+	protected String outlineStringRepresentation(IOutlineNode node) {
+		return super.outlineStringRepresentation(node).replace("\r", "");
+	}
+
 	@Test
 	public void testEmptyOutline() throws Exception {
 		assertAllLabels("\t\t\t", "");
@@ -29,20 +38,20 @@ public class XklaimOutlineTest extends AbstractOutlineTest {
 	@Test
 	public void testOutline() throws Exception {
 		this.assertAllLabels("""
-			package mydsl
-			
-			proc MyProc(String s) {
-				
-			}
-			net HelloNet physical "localhost:9999" {
-				node Hello1 {
-					println("Hello World!")
+				package mydsl
+
+				proc MyProc(String s) {
+
 				}
-				node Hello2 {
-					println("Hello World!")
+				net HelloNet physical "localhost:9999" {
+					node Hello1 {
+						println("Hello World!")
+					}
+					node Hello2 {
+						println("Hello World!")
+					}
 				}
-			}
-			""", """
+				""", """
 				mydsl
 				  MyProc
 				  HelloNet
