b2f94042a976c4f595cdf2a6739270117c907929
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2026-06-01 16:55:29 +0200

    Refactor XklaimWorkbenchTest to extend AbstractWorkbenchTest and streamline project creation

--- xklaim/xklaim.parent/xklaim.ui.tests/src/xklaim/ui/tests/XklaimWorkbenchTest.java
+++ xklaim/xklaim.parent/xklaim.ui.tests/src/xklaim/ui/tests/XklaimWorkbenchTest.java
@@ -1,11 +1,8 @@
 package xklaim.ui.tests;
 
-import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.addBuilder;
-import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.addNature;
-import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.cleanWorkspace;
 import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.createFile;
+import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.monitor;
 import static org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil.waitForBuild;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
@@ -14,47 +11,23 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.Path;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.XtextRunner;
 import org.eclipse.xtext.ui.XtextProjectHelper;
-import org.eclipse.xtext.ui.testing.util.JavaProjectSetupUtil;
-import org.junit.BeforeClass;
+import org.eclipse.xtext.ui.testing.AbstractWorkbenchTest;
+import org.eclipse.xtext.ui.util.PluginProjectFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(XtextRunner.class)
 @InjectWith(XklaimUiInjectorProvider.class)
-public class XklaimWorkbenchTest {
+public class XklaimWorkbenchTest extends AbstractWorkbenchTest {
 	private static final String TEST_PROJECT = "xklaim.ui.tests.project";
 	private static final String TEST_FILE = "TestFile";
 	private static final String PLUGIN_NATURE = "org.eclipse.pde.PluginNature";
-
-	private static IProject project;
-
-	@BeforeClass
-	public static void createTestProject() throws Exception {
-		cleanWorkspace();
-		var javaProject = JavaProjectSetupUtil.createJavaProject(TEST_PROJECT);
-		project = javaProject.getProject();
-		JavaProjectSetupUtil.addSourceFolder(javaProject, "src-gen");
-		JavaProjectSetupUtil.addToClasspath(javaProject,
-				JavaCore.newContainerEntry(new Path("org.eclipse.pde.core.requiredPlugins")));
-		addNature(project, PLUGIN_NATURE);
-		addNature(project, XtextProjectHelper.NATURE_ID);
-		addBuilder(project, XtextProjectHelper.BUILDER_ID);
-		createFile(TEST_PROJECT + "/META-INF/MANIFEST.MF", """
-				Manifest-Version: 1.0
-				Bundle-ManifestVersion: 2
-				Bundle-Name: xklaim.ui.tests.project
-				Bundle-SymbolicName: xklaim.ui.tests.project
-				Bundle-Version: 1.0.0.qualifier
-				Require-Bundle: xklaim.runtime
-				Bundle-RequiredExecutionEnvironment: JavaSE-21
-				""");
-		waitForBuild();
-	}
 
 	public IFile createTestFile(CharSequence contents) throws Exception {
 		return createFile(TEST_PROJECT + "/src/" + TEST_FILE + ".xklaim", contents.toString());
@@ -62,6 +35,7 @@ public class XklaimWorkbenchTest {
 
 	@Test
 	public void testErrorInGeneratedJavaCodeIsCopiedToOriginalFile() throws Exception {
+		createTestProject();
 		var source = createTestFile("""
 				net TestNet physical "tcp-127.0.0.1:9999" {
 					node Reader logical "reader" {
@@ -89,5 +63,19 @@ public class XklaimWorkbenchTest {
 
 	private static String getMessage(IMarker marker) {
 		return marker.getAttribute(IMarker.MESSAGE, "");
+	}
+
+	private IProject createTestProject() {
+		var projectFactory = new PluginProjectFactory();
+		projectFactory.setWorkspace(ResourcesPlugin.getWorkspace());
+		projectFactory.setWorkbench(PlatformUI.getWorkbench());
+		projectFactory.setProjectName(TEST_PROJECT);
+		projectFactory.addProjectNatures(JavaCore.NATURE_ID, PLUGIN_NATURE, XtextProjectHelper.NATURE_ID);
+		projectFactory.addBuilderIds(JavaCore.BUILDER_ID, XtextProjectHelper.BUILDER_ID);
+		projectFactory.addFolders(Arrays.asList("src", "src-gen"));
+		projectFactory.getRequiredBundles().add("xklaim.runtime");
+		var project = projectFactory.createProject(monitor(), null);
+		waitForBuild();
+		return project;
 	}
 }
